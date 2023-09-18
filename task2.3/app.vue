53dc5ad4913b0d0d04aaa5204dadd3113e9e7cc9
flynnholland <87583341+flynnholland@users.noreply.github.com>
2023-09-19 00:10:28 +1000

    Create app.vue

---
+++ task2.3/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ message }}</p>
+  <div v-html="rawHtml"></div>
+  <div v-bind:id="dynamicId"></div>
+  <p>{{ message.split('').reverse().join('') }}</p>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const message = ref('Task 2.3C');
+  const rawHtml = '<span style="color: red">Red Raw HTML</span>';
+  const dynamicId = 'my-id';
+</script>
